b5c9e34c265744e0d59ec57a94ca32162c973562
a7md1742 <67839658+a7md1742@users.noreply.github.com>
2020-09-16 15:59:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var a = 5
+var t = 3
+console.log (a)
+
+// primitive data -- simple data
+var number1 = 1; // numbers
+var stringName = 'sadfasdf'; // string
+var booleanTrue = true; // boolean
+
+// wrapper data
+var ahmad = {
+    name : 'ahmad yagoub',
+    age : 17,
+    study : () => {
+        console.log("my name is: "+ahmad.name+" my age is: "+ahmad.age)
+    }
+}
+
+// functions
+function log(string){
+    console.log(string)
+}
+
+log('im ahmad')
